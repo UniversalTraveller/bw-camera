2d15ffa5a9d6779a0686c2898ec73967d1e65572
Andreas Kiener <andreas.kiener@gmail.com>
2025-05-01 23:58:13 +0200

    1 Add camera

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 export default function App() {
+  
+  const { hasPermission, requestPermission } = useCameraPermission()
+  
+  if (!hasPermission) {
+    requestPermission()
+  }
+  
+  const camera = useCameraDevice('back')
+
+  if (camera === undefined) {
+    return
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Camera
+      style={StyleSheet.absoluteFill}
+      device={camera}
+      isActive={true}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
