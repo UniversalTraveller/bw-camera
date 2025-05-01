a45115453161fe47907ea7b96be49144ae1ebbb3
Andreas Kiener <andreas.kiener@gmail.com>
2025-05-02 01:30:11 +0200

    Add ESLint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    // We can add specific rules here later
+  },
+}
