a6565c4e41bdeb2a7d83bf5373bd39cd0f783591
Andreas Kiener <andreas.kiener@gmail.com>
2025-05-02 20:05:34 +0200

    1 Format after merging prettier config

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
-import { StyleSheet } from 'react-native';
-import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
+import { StyleSheet } from 'react-native'
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
 
 export default function App() {
-  
   const { hasPermission, requestPermission } = useCameraPermission()
-  
+
   if (!hasPermission) {
     requestPermission()
   }
-  
+
   const camera = useCameraDevice('back')
 
   if (camera === undefined) {
     return
   }
 
-  return (
-      <Camera
-      style={StyleSheet.absoluteFill}
-      device={camera}
-      isActive={true}
-    />
-  );
+  return <Camera style={StyleSheet.absoluteFill} device={camera} isActive={true} />
 }
-
-
