4ccfa2b98c86ac38c837c1365a0b14a76ae24d8f
Andreas Kiener <andreas.kiener@gmail.com>
2025-05-05 21:49:00 +0200

    Shutter button feedback

--- src/ShutterButton.tsx
+++ src/ShutterButton.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
+
 import { IconButton } from 'react-native-paper'
 
-const ShutterButton = ({ onShutterPress }: { onShutterPress: () => void }) => {
+const ShutterButton = ({ onShutterPress }: { onShutterPress: () => Promise<void> }) => {
+  const [isPressed, setIsPressed] = useState(false)
+
+  const onPress = async () => {
+    setIsPressed(true)
+
+    await onShutterPress()
+
+    setIsPressed(false)
+  }
+
   return (
     <IconButton
       icon="camera"
       size={48}
-      onPress={onShutterPress}
+      onPress={onPress}
       mode="contained-tonal"
       style={{ marginBottom: 60 }}
+      selected={isPressed}
     />
   )
 }
