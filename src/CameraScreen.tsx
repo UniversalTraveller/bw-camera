7853bb5348bde024c5e8859da147be48888f4014
Andreas Kiener <andreas.kiener@gmail.com>
2025-05-05 21:00:36 +0200

    Simulate mirror

--- src/CameraScreen.tsx
+++ src/CameraScreen.tsx
@@ -1,13 +1,17 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+
+import { View } from 'react-native'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Camera, useCameraDevice, useCameraPermission, PhotoFile } from 'react-native-vision-camera'
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
 import * as MediaLibrary from 'expo-media-library'
-
-import ShutterButton from './ShutterButton'
 import { useAppState } from '@react-native-community/hooks'
 
+import ShutterButton from './ShutterButton'
+
 const CameraScreen = () => {
+  const [showMirrorOverlay, setShowMirrorOverlay] = useState(false)
+
   const appState = useAppState()
 
   const isActive = appState === 'active'
@@ -31,7 +35,11 @@ const CameraScreen = () => {
   }
 
   const onShutterPress = async () => {
+    setShowMirrorOverlay(true)
+
     const photo = await camera.current?.takePhoto()
+
+    setShowMirrorOverlay(false)
 
     if (!photo) {
       return
@@ -55,14 +63,31 @@ const CameraScreen = () => {
         alignItems: 'center'
       }}
     >
-      <Camera
-        ref={camera}
-        style={{ flex: 1, width: '100%', backgroundColor: 'red' }}
-        device={cameraDevice}
-        isActive={isActive}
-        resizeMode="contain"
-        photo={true}
-      />
+      <View style={{ flex: 1, width: '100%' }}>
+        <Camera
+          ref={camera}
+          style={{ flex: 1 }}
+          device={cameraDevice}
+          isActive={isActive}
+          resizeMode="contain"
+          photo={true}
+        />
+
+        {showMirrorOverlay && (
+          <View
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              backgroundColor: 'black',
+              zIndex: 10
+            }}
+            pointerEvents="none"
+          />
+        )}
+      </View>
       <ShutterButton onShutterPress={onShutterPress} />
     </SafeAreaView>
   )
