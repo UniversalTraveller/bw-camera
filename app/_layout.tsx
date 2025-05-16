aa4af3c7680fe8dd7d545f8460248b8db59cd72c
Andreas Kiener <59486612+UniversalTraveller@users.noreply.github.com>
2025-05-16 22:46:38 +0200

    Save settings to local storage (#31)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,45 +2,48 @@ import { Stack } from 'expo-router'
 import { PaperProvider } from 'react-native-paper'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { appTheme, useAppTheme } from '../theme/appTheme'
-import { store } from '../store'
+import { store, persistor } from '../store'
 
 export default function Layout() {
   const { colors } = useAppTheme()
 
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <PaperProvider theme={appTheme}>
-          <Stack
-            screenOptions={{
-              contentStyle: { backgroundColor: colors.background }
-            }}
-          >
-            <Stack.Screen
-              name="index"
-              options={{
-                headerShown: false
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <PaperProvider theme={appTheme}>
+            <Stack
+              screenOptions={{
+                contentStyle: { backgroundColor: colors.background }
               }}
-            />
+            >
+              <Stack.Screen
+                name="index"
+                options={{
+                  headerShown: false
+                }}
+              />
 
-            <Stack.Screen
-              name="settings"
-              options={{
-                title: 'Settings',
-                headerStyle: {
-                  backgroundColor: colors.background
-                },
-                headerTintColor: colors.onSurface,
-                headerTitleStyle: {
-                  color: colors.onSurface
-                }
-              }}
-            />
-          </Stack>
-        </PaperProvider>
-      </SafeAreaProvider>
+              <Stack.Screen
+                name="settings"
+                options={{
+                  title: 'Settings',
+                  headerStyle: {
+                    backgroundColor: colors.background
+                  },
+                  headerTintColor: colors.onSurface,
+                  headerTitleStyle: {
+                    color: colors.onSurface
+                  }
+                }}
+              />
+            </Stack>
+          </PaperProvider>
+        </SafeAreaProvider>
+      </PersistGate>
     </Provider>
   )
 }
