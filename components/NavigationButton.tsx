46df6061a1eb0f7fe9db24527471fb2e07dab53e
Andreas Kiener <59486612+UniversalTraveller@users.noreply.github.com>
2025-05-17 23:23:04 +0200

    Prevent double navigation (#41)

--- components/NavigationButton.tsx
+++ components/NavigationButton.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react'
+
 import { IconButton } from 'react-native-paper'
+import { useIsFocused } from '@react-navigation/native'
 
 const NavigationButton = ({ onPress, icon }: { onPress: () => void; icon: string }) => {
-  return <IconButton icon={icon} size={32} onPress={onPress} />
+  const [isNavigating, setIsNavigating] = useState(false)
+  const isFocused = useIsFocused()
+
+  useEffect(() => {
+    if (isFocused) {
+      setIsNavigating(false)
+    }
+  }, [isFocused])
+
+  const handlePress = () => {
+    if (isNavigating) return
+    setIsNavigating(true)
+    onPress()
+  }
+
+  return <IconButton icon={icon} size={32} onPress={handlePress} disabled={isNavigating} />
 }
 
 export default NavigationButton
